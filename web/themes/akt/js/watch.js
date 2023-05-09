fcecde2215ac503c949b78a25db870b85f94f57e
Isaac Bigsby Trogdon <i.trogdon@loom.de>
2023-05-09 09:02:51 +0200

    Add akt theme

---
+++ web/themes/akt/js/watch.js
@@ -0,0 +1,35 @@
+const chokidar = require('chokidar');
+const fs = require("fs");
+const { exec } = require("child_process");
+const { renderStylusCSS } = require('./compile');
+const log = console.log.bind(console);
+
+const args = process.argv.slice(2);
+if (!args.length) return;
+
+try {
+  // pre-compile
+  exec("yarn compile", (error, stdout, stderr) => {
+    if (error) {
+      console.log(`error: ${error.message}`);
+      return;
+    }
+    if (stderr) {
+      console.log(`stderr: ${stderr}`);
+      return;
+    }
+    console.log(`stdout: ${stdout}`);
+  });
+
+  const directories = args.map((dir, i) => `./${dir}`)
+    .filter((dir) => fs.existsSync(dir));
+  log(`Begin watching: ${directories}`);
+  const watcher = chokidar.watch(directories, {
+    ignored: /(^|[\/\\])\../, // ignore dotfiles
+  }).on('change', pathToFile => {
+    const destinationPath = pathToFile.replace('stylus', 'css');//@todo - add to options?
+    renderStylusCSS(pathToFile, destinationPath)
+  });
+} catch (e) {
+  console.error(e);
+}
