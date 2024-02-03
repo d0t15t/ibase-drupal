0d0efe1a3c7a6f5f17432f7ab9be5271ce5404f9
Dyss Admin <ja@dyss.net>
2024-02-03 05:26:09 +0000

    Add zeigen them.

---
+++ web/themes/zeigen/js/zeigen.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * Zeigen behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.zeigen = {
+    attach: function (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (Drupal));
