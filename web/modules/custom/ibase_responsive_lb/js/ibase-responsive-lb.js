96404791a5c45eec4f2af4456dd88725d71d3621
Isaac Bigsby Trogdon <i.trogdon@loom.de>
2024-04-17 08:53:01 +0200

    Exhibitions responsive.

--- web/modules/custom/ibase_responsive_lb/js/ibase-responsive-lb.js
+++ web/modules/custom/ibase_responsive_lb/js/ibase-responsive-lb.js
@@ -3,6 +3,9 @@
   Drupal.behaviors.breakpoints = {
     attach: function (context, settings) {
 
+      // if url is admin, return
+      if (window.location.href.indexOf('admin') > -1) { return; }
+
       const set = settings.responsive.breakpoints;
       let breakpoints = {};
 
@@ -10,8 +13,6 @@
         let newKey = key.split('.')[1]; // split the key by '.' and take the second part
         breakpoints[newKey] = set[key];
       }
-      // remove 'extra_small' from the array
-      // delete breakpoints.extra_small;
 
       function getMatches(breakpoints) {
         // return an array of matching breakpoints
@@ -38,22 +39,29 @@
         const matched = matches.reduce((acc, key) => acc.concat(map[key]), [])
         // find all the unmatched as well - it should contain no duplicates and must not contain the matched value.
         const unmatched = Object.values(map).reduce((acc, val) => acc.concat(val), []).filter((val, index, self) => !matched.includes(val) && self.indexOf(val) === index)
+        console.log('matches', matches, 'matched', matched, 'unmatched', unmatched)
+
         matched.forEach((classname) => {
-          const selected = document.querySelector(`.` + classname)
-          if (selected) {
-            selected.classList.remove('visually-hidden')
-            // console.log('Showing: ' + classname)
+          const selected = document.querySelectorAll(`.` + classname)
+          if (selected !== undefined) {
+            selected.forEach((item) => {
+              item.classList.remove('visually-hidden')
+              console.log('Showing: ' + item)
+            })
+            // selected.classList.remove('visually-hidden')
           }
         })
         unmatched.forEach((classname) => {
-          const selected = document.querySelector(`.` + classname)
-          if (selected) {
-            selected.classList.add('visually-hidden')
-            // console.log('Hiding: ' + classname)
+          const selected = document.querySelectorAll(`.` + classname)
+          if (selected !== undefined) {
+            selected.forEach((item) => {
+              item.classList.add('visually-hidden')
+              console.log('Hiding: ' + item)
+            })
           }
         })
 
-        console.log('matches', matches, 'matched', matched, 'unmatched', unmatched)
+
       }
 
       doLayout(breakpoints);
@@ -61,9 +69,9 @@
       // @TODO disable on admin pages
       window.addEventListener('resize', () => {
         debounce(() => {
-          doLayout(breakpoints);
+          // doLayout(breakpoints);
         }, 50, false)
-        // doLayout(breakpoints);
+        doLayout(breakpoints);
 
       });
 
