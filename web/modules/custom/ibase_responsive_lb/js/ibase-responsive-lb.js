c705c33007ca86637c1a2cacae593c69e29fa9f5
Isaac Bigsby Trogdon <i.trogdon@loom.de>
2024-05-07 14:06:12 +0200

    Update zeigen

--- web/modules/custom/ibase_responsive_lb/js/ibase-responsive-lb.js
+++ web/modules/custom/ibase_responsive_lb/js/ibase-responsive-lb.js
@@ -14,15 +14,19 @@
         breakpoints[newKey] = set[key];
       }
 
+      /**
+       * Get the breakpoints that match the media query
+       *
+       * @param breakpoints
+       * @returns {*[]}
+       */
       function getMatches(breakpoints) {
-        // return an array of matching breakpoints
         const matches = [];
         for (let key in breakpoints) {
           if (window.matchMedia(breakpoints[key]).matches) {
             matches.push(key);
           }
         }
-
         return matches;
       }
 
@@ -39,16 +43,28 @@
         const matched = matches.reduce((acc, key) => acc.concat(map[key]), [])
         // find all the unmatched as well - it should contain no duplicates and must not contain the matched value.
         const unmatched = Object.values(map).reduce((acc, val) => acc.concat(val), []).filter((val, index, self) => !matched.includes(val) && self.indexOf(val) === index)
-        console.log('matches', matches, 'matched', matched, 'unmatched', unmatched)
+        // console.log('matches', matches, 'matched', matched, 'unmatched', unmatched)
+
+        const layout = document.querySelector('main .layout')
+
+        // escape if there are not enough elements to switch between.
+        if (layout.parentElement.children.length < 2) {
+          return;
+        }
+        const url = window.location.href;
+        const uri = window.location.pathname;
+        const uria = uri.split('/').slice(1)
+
+        if (uria[0] === 'admin') return
+        if (uria[0] === 'node' && uria[2] === 'layout') return
 
         matched.forEach((classname) => {
           const selected = document.querySelectorAll(`.` + classname)
           if (selected !== undefined) {
             selected.forEach((item) => {
               item.classList.remove('visually-hidden')
-              console.log('Showing: ' + item)
+              // console.log('Showing: ' + item)
             })
-            // selected.classList.remove('visually-hidden')
           }
         })
         unmatched.forEach((classname) => {
@@ -56,14 +72,13 @@
           if (selected !== undefined) {
             selected.forEach((item) => {
               item.classList.add('visually-hidden')
-              console.log('Hiding: ' + item)
+              // console.log('Hiding: ' + item)
             })
           }
         })
 
 
       }
-
       doLayout(breakpoints);
 
       // @TODO disable on admin pages
